aedb855e233b6f672cf8ee8d5d99cc2a82585db8
varunsriva <varunsri282@gmail.com>
2021-08-06 19:19:36 +0530

    fdoc-1

--- CountWord.js
+++ CountWord.js
@@ -48,3 +48,16 @@ for (var i = 0; i < 7; i++) {
 }
 
 
+
+
+// 2.b. Write a function which returns array of seven random numbers in a range of 0-9. All the numbers must be unique
+
+// sevenRandomNumbers()
+// [1,4,5,7,9,8,0]
+
+var arr = [];
+while(arr.length < 7){
+    var r = Math.floor(Math.random() * 9) + 1;
+    if(arr.indexOf(r) === -1) arr.push(r);
+}
+console.log(arr);

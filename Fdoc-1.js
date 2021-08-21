347244a2a7efc56b4425843992cb00cae0149039
varunsriva <varunsri282@gmail.com>
2021-08-21 21:19:33 +0530

    fdoc-1

--- Fdoc-1.js
+++ Fdoc-1.js
@@ -1,7 +1,7 @@
 //Problem Statement
 //1.a. Write a function which count the number of occurrence of words in a paragraph or a sentence.The function `countWords` takes a paragraph and two words as parameters. It compare  which word is most frequently occurred in the paragraph.
 const paragraph = 'I love teaching. If you do not love teaching what else can you love. I love Javascript if you do not love something which can give life to your appication what else can you love.';
-function countWords(paragraph, word1, word2){
+function countWords(paragraph, love, you){
 let count1 = 0;
 let count2 = 0; 
 //Split method applying in input to put the strings into an array of substrings

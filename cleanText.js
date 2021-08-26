e3639ec052a6a1873c657d7692abd3b4b8c9833b
varunsriva <varunsri282@gmail.com>
2021-08-26 22:34:20 +0530

    solution 1b

---
+++ cleanText.js
@@ -0,0 +1,9 @@
+function cleanText(sentence){
+
+    return sentence.replace(/[^a-zA-Z. ]/g, "");
+    
+     }
+    let sentence ="%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher";
+    
+    
+    console.log(cleanText(sentence));
